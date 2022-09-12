6c93a9db2c26d58939c4f3ce71d0d5c94c82bf3d
Nicolau Riess <nicolauriess@gmail.com>
2022-09-11 22:26:54 -0300

    style: develop some storybook component

---
+++ src/components/Top/GameName.tsx
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export interface GameNameProps {
+  children: string;
+}
+
+export const GameName = styled.h1<GameNameProps>`
+  font-size: 2em;
+`;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
-import { Legend } from './components/Legend';
+import { Legend } from './components/Top/Legend';
 
 const root = ReactDOMClient.createRoot(
   document.getElementById('root') as HTMLElement,
