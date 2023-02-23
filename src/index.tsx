d34e26c7d4fb46a11be63c329ed1df35621dc6ce
Nicolau Riess <nicolauriess@gmail.com>
2023-02-23 18:27:19 -0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOMClient.createRoot(
 root.render(
   <>
     <Top feature="Flag" firstAction="ctrl" secondAction="click">
-      Minesweeper123
+      Minesweeper - staging Minesweeper - trying GitHub Actions
     </Top>
     <Scoreboard
       time="000"
